50608ff338df4a49920e5ba31dd6ec29c33e94b6
Ethan McAuliffe <ethan@mcauliffe-657.local>
2017-05-29 08:36:31 -0400

    completed test cases

--- ExamPrepGoodTimes/main.swift
+++ ExamPrepGoodTimes/main.swift
@@ -1,73 +1,76 @@
 import Foundation
 
 /*
- 
- ORGANIZING YOUR SOLUTION
- 
- A good way to orgaize your code is to separate your code into the three sections - input, process, output – as much as possible.
- 
- The start of a solution is implemented below. Consider all the possible inputs. Can you finish the solution?
- 
- */
+
+ORGANIZING YOUR SOLUTION
+
+A good way to orgaize your code is to separate your code into the three sections - input, process, output – as much as possible.
+
+The start of a solution is implemented below. Consider all the possible inputs. Can you finish the solution?
+
+*/
 
 /*
- 
- INPUT
- 
- Be sure that your implementation of this section discards all the possible bad inputs the user could provide.
- 
- Make use of your test plan and algorithm to ensure your code is complete.
- 
- */
-var inputToProcess : String = ""
 
-// Loop until valid input is received
-while inputToProcess == "" {
-    
-    // Show the prompt
-    print("Ask the question here? ", terminator: "")
-    
-    // Get the user's input
-    var input : String?
-    input = readLine()
-    
-    // Use optional binding to see if the string can be unwrapped (to see if it is not nil)
-    if let notNilInput = input {
-        
-        // You probably need to add additional checks to be sure the
-        // input received is valid
-        // Add checks as needed...
-        
-        // Save the input given, as we are certain it's what we are looking for now
-        inputToProcess = notNilInput
-        
-    }
-    
-}
+INPUT
+
+Be sure that your implementation of this section discards all the possible bad inputs the user could provide.
+
+Make use of your test plan and algorithm to ensure your code is complete.
+
+*/
+
+var inputToProcess = Int()
+
+
+inputToProcess = 1421
+
+//// Loop until valid input is received
+//
+//// Show the prompt
+//print("Ask the question here? ", terminator: "")
+//
+//// Get the user's input
+//var input : String?
+//input = readLine()
+//
+//// Use optional binding to see if the string can be unwrapped (to see if it is not nil)
+//if let notNilInput = input {
+//	if let isAnInteger = Int(notNilInput){
+//		
+//	} else {
+//		
+//	}
+//}
 
 /*
- 
- PROCESS
- 
- Here is where you implement the logic that solves the problem at hand.
- 
- Make use of your test plan and algorithm to ensure your code is complete.
- 
- */
+
+PROCESS
+
+Here is where you implement the logic that solves the problem at hand.
+
+Make use of your test plan and algorithm to ensure your code is complete.
+
+*/
 
 // Add 'process' code below....
-print("replace with process logic")
 
 
 /*
- 
- OUTPUT
- 
- Here is where you report the results of the 'process' section above.
- 
- Make use of your test plan and algorithm to ensure your code is complete.
- 
- */
+
+OUTPUT
+
+Here is where you report the results of the 'process' section above.
+
+Make use of your test plan and algorithm to ensure your code is complete.
+
+*/
 
 // Add 'output' code below... replace what is here as needed.
-print("The input given was: \(inputToProcess)")
+print(cities[0])
+print(cities[1])
+print(cities[2])
+print(cities[3])
+print(cities[4])
+print(cities[5])
+print(cities[6])
